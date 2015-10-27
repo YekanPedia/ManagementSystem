22459a10bc03b0e582bfdfbba2a73c11f52a69c8
mohammadKarimi <dot.dotbydot@gmail.com>
2015-10-26 17:38:36 -0700

    change service layer to .net 4.6 add oauth2authenticaton secretId and Secret Key

---
+++ YekanPedia.ManagementSystem.Console/AppSetting.cs
@@ -0,0 +1,10 @@
+﻿ namespace YekanPedia.ManagementSystem.Console
+{
+    using System.Configuration;
+    public static class AppSettings {
+        public static string GoogleClientId => ConfigurationManager.AppSettings["GoogleClientId"];
+        public static string GoogleClientSecret => ConfigurationManager.AppSettings["GoogleClientSecret"];
+        public static bool ClientValidationEnabled => bool.Parse(ConfigurationManager.AppSettings["ClientValidationEnabled"]);
+        public static bool UnobtrusiveJavaScriptEnabled => bool.Parse(ConfigurationManager.AppSettings["UnobtrusiveJavaScriptEnabled"]);
+    }
+}
