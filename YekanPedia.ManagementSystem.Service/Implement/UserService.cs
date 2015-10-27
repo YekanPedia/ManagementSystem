0be50ea60b55daa83255fdb048ca5fe1ba15297d
mohammadKarimi <dot.dotbydot@gmail.com>
2015-10-26 17:58:10 -0700

    add user service and interface

---
+++ YekanPedia.ManagementSystem.Service/Implement/UserService.cs
@@ -0,0 +1,41 @@
+﻿namespace YekanPedia.ManagementSystem.Service.Implement
+{
+    using System;
+    using System.Collections.Generic;
+    using Interfaces;
+    using Domain.Entity;
+    using Data.Conext;
+    using System.Data.Entity;
+
+    public class UserService : IUserService
+    {
+        #region Constructur
+        private readonly IUnitOfWork _uow;
+        private readonly IDbSet<User> _user;
+        public UserService(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _user = uow.Set<User>();
+        }
+        #endregion
+        public void AddUser(User model)
+        {
+            model.IsActive = true;
+            model.LastLoginDate = DateTime.Now;
+            _user.Add(model);
+            _uow.SaveChanges();
+        }
+
+        public void ChangeUserState(int userId, bool state = true)
+        {
+
+        }
+
+        public IEnumerable<User> GetUserList()
+        {
+            throw new NotImplementedException();
+        }
+        public User Find(int userId) => _user.Find(userId);
+
+    }
+}
---
+++ YekanPedia.ManagementSystem.Service/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿namespace YekanPedia.ManagementSystem.Service.Interfaces
+{
+    using System.Collections.Generic;
+    using Domain.Entity;
+
+    public interface IUserService
+    {
+        void AddUser(User model);
+        void ChangeUserState(int userId, bool state = true);
+        IEnumerable<User> GetUserList();
+    }
+}
