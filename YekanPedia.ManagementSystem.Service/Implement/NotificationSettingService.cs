87ebc0d1783b9085bee0a8ca3cca7c10c44296b7
mohammadKarimi <dot.dotbydot@gmail.com>
2015-11-16 08:19:32 +0330

    include notificationSettingService

---
+++ YekanPedia.ManagementSystem.Service/Implement/NotificationSettingService.cs
@@ -0,0 +1,51 @@
+﻿namespace YekanPedia.ManagementSystem.Service.Implement
+{
+    using System.Collections.Generic;
+    using Interfaces;
+    using Domain.Entity;
+    using Data.Conext;
+    using System.Data.Entity;
+    using System.Linq;
+    using System;
+    using ExternalService.Interfaces;
+    using ExternalService.MessagingGateway;
+    using InfraStructure;
+    using Properties;
+
+    public class NotificationSettingService : INotificationSettingService
+    {
+        #region Constructur
+        readonly IUnitOfWork _uow;
+        readonly IDbSet<NotificationSetting> _notification;
+        readonly IUserService _userService;
+        readonly IMessagingGatewayAdapter _messagingGateway;
+        public NotificationSettingService(IUnitOfWork uow, IUserService userService, IMessagingGatewayAdapter messagingGateway)
+        {
+            _uow = uow;
+            _notification = uow.Set<NotificationSetting>();
+            _userService = userService;
+            _messagingGateway = messagingGateway;
+        }
+        #endregion
+        public IEnumerable<NotificationSetting> GetAllNotification()
+        {
+            return _notification.AsNoTracking().ToList();
+        }
+        public NotificationSetting GetNotificationType(NotificationType type)
+        {
+            return _notification.Where(X => X.NotificationType == type).AsNoTracking().FirstOrDefault();
+        }
+        public IServiceResults<int> Edit(NotificationSetting model)
+        {
+            _notification.Attach(model);
+            _uow.Entry(model).State = EntityState.Modified;
+            var resultSave = _uow.SaveChanges();
+            return new ServiceResults<int>
+            {
+                IsSuccessfull = resultSave.ToBool(),
+                Message = resultSave.ToMessage(BusinessMessage.Error),
+                Result = model.NotificationSettingId
+            };
+        }
+    }
+}
---
+++ YekanPedia.ManagementSystem.Service/Interfaces/INotificationSettingService.cs
@@ -0,0 +1,14 @@
+﻿namespace YekanPedia.ManagementSystem.Service.Interfaces
+{
+    using Domain.Entity;
+    using System.Collections.Generic;
+    using System;
+    using InfraStructure;
+
+    public interface INotificationSettingService
+    {
+        IEnumerable<NotificationSetting> GetAllNotification();
+        IServiceResults<int> Edit(NotificationSetting model);
+        NotificationSetting GetNotificationType(NotificationType type);
+    }
+}
