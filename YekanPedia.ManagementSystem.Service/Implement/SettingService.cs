da16e6ab23dbe80cfbb81ba64dc45ff90834cbeb
mohammadKarimi <dot.dotbydot@gmail.com>
2015-12-03 11:49:40 +0330

    add setting Service

---
+++ YekanPedia.ManagementSystem.Service/Implement/SettingService.cs
@@ -0,0 +1,25 @@
+﻿namespace YekanPedia.ManagementSystem.Service.Implement
+{
+    using System.Linq;
+    using Interfaces;
+    using Domain.Entity;
+    using Data.Conext;
+    using System.Data.Entity;
+
+    public class SettingService : ISettingService
+    {
+        #region Constructur
+        readonly IUnitOfWork _uow;
+        readonly IDbSet<Setting> _setting;
+        public SettingService(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _setting = uow.Set<Setting>();
+        }
+        #endregion
+        public Setting GetDefaultSetting()
+        {
+            return _setting.FirstOrDefault(X => X.SettingId == 1);
+        }
+    }
+}
