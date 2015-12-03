39a9bd7f26eb646170755f3551d1d7db6eea8fdb
mohammadKarimi <dot.dotbydot@gmail.com>
2015-12-03 11:48:59 +0330

    add setting Table

--- YekanPedia.ManagementSystem.Domain/Entity/Setting.cs
+++ YekanPedia.ManagementSystem.Domain/Entity/Setting.cs
@@ -1,11 +1,21 @@
 ﻿namespace YekanPedia.ManagementSystem.Domain.Entity
 {
-    using System;
     using System.ComponentModel.DataAnnotations;
+    using Properties;
+    using System.ComponentModel.DataAnnotations.Schema;
 
+    [Table(nameof(Setting), Schema = "Base")]
     public class Setting
     {
         [Key]
-        public Guid SettingId { get; set; }
+        public int SettingId { get; set; }
+
+        [Range(1, 100, ErrorMessage = nameof(FilesPersistance), ErrorMessageResourceType = typeof(DisplayError))]
+        public int FilesPersistance { get; set; }
+
+        [Display(ResourceType = typeof(DisplayNames), Name = nameof(BirthDateText))]
+        [MaxLength(300, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [StringLength(300, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        public string BirthDateText { get; set; }
     }
 }
