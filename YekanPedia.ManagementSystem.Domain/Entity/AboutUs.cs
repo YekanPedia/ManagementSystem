7c7870f7a55655b26397b21079438ac48e9d5ab0
mohammadKarimi <dot.dotbydot@gmail.com>
2015-12-03 11:49:11 +0330

    add aboutUs Table

---
+++ YekanPedia.ManagementSystem.Domain/Entity/AboutUs.cs
@@ -0,0 +1,67 @@
+﻿namespace YekanPedia.ManagementSystem.Domain.Entity
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Properties;
+
+    [Table(nameof(AboutUs), Schema = "Info")]
+    public class AboutUs
+    {
+        [Key]
+        public int AboutUsId { get; set; }
+
+        [MaxLength(30, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        public string CompanyName { get; set; }
+
+        [MaxLength(20, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        public string Telphone { get; set; }
+
+        [MaxLength(50, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        public string Email { get; set; }
+
+        [MaxLength(150, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        public string Address { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [MaxLength(20, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [StringLength(20, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        public string Latitude { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [MaxLength(20, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [StringLength(20, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        public string Longitude { get; set; }
+
+        [Display(ResourceType = typeof(DisplayNames), Name = nameof(Twitter))]
+        [MaxLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [StringLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [Column(TypeName = "varchar")]
+        public string Twitter { get; set; }
+
+        [Display(ResourceType = typeof(DisplayNames), Name = nameof(Facebook))]
+        [MaxLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [StringLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [Column(TypeName = "varchar")]
+        public string Facebook { get; set; }
+
+        [Display(ResourceType = typeof(DisplayNames), Name = nameof(GooglePlus))]
+        [MaxLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [StringLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [Column(TypeName = "varchar")]
+        public string GooglePlus { get; set; }
+
+        [Display(ResourceType = typeof(DisplayNames), Name = nameof(LinkedIn))]
+        [MaxLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [StringLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [Column(TypeName = "varchar")]
+        public string LinkedIn { get; set; }
+
+        [Display(ResourceType = typeof(DisplayNames), Name = nameof(Telegram))]
+        [MaxLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [StringLength(90, ErrorMessageResourceName = nameof(DisplayError.MaxLength), ErrorMessageResourceType = typeof(DisplayError))]
+        [Column(TypeName = "varchar")]
+        public string Telegram { get; set; }
+    }
+}
